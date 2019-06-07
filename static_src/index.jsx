8a5fc5c6ab9d1136c9b9e8a135a7e4e9ef41f4b5
Miro2018 <ilmir2007@inbox.ru>
2019-06-07 17:32:32 +0300

    д.з за 1-ый урок

---
+++ static_src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const component = <h1>component</h1>;
+
+ReactDOM.render(
+    component,
+    document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const webpack = require('webpack');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  entry: {
+    app: './index.jsx',
+  },
+  context: `${__dirname}/static_src`,
+  output: {
+    path: `${__dirname}/static/build`,
+    filename: NODE_ENV === 'development' ? '[name].js' : '[name]-[hash].js',
+    publicPath: '/static/build/',
+  },
+
+  optimization: {
+    minimize: NODE_ENV !== 'development',
+  },
+
+  // plugins: [
+  //   new webpack.NoEmitOnErrorsPlugin(),
+  // ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(scss|sass)$/,
+        // loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded,modules: true',
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              modules: true
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg|svg|ttf|eot|woff|woff2)$/,
+        loader: 'url-loader?limit=9998192&name=[path][name].[ext]',
+      },
+      // {
+      //   test: /\.(jpg)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         name: '[path][name].[ext]',
+      //       },
+      //     },
+      //   ],
+      // }
+    ],
+  },
+
+  resolve: {
+    modules: [`${__dirname}/static_src`, 'node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+
+  watch: NODE_ENV === 'development',
+  watchOptions: {
+    aggregateTimeout: 100,
+  },
+
+  devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
+};
